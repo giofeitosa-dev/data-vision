d1e2bf401cee2c5ad39f8862ad7aab61a6ddecee
Giovani <giovanifeitosa@gmail.com>
2026-08-03 11:20:17 -0300

    chore: scaffold Spring Boot backend with Maven wrapper

---
+++ backend/src/test/java/com/datavision/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datavision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
